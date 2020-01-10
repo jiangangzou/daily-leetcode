6e13731825451ac53d2f9875ea0d586dc3dcd3b6
1083140621@qq.com <1083140621@qq.com>
2020-01-10 21:45:45 +0800

    [add]新增二分查找

--- sort.js
+++ sort.js
@@ -113,4 +113,19 @@ var quickSort = function(arr) {
     return quickSort(left).concat([pivot],quickSort2(right))
 }
 
-// 桶排序
+// 二分查找
+function binarySearch(arr, target) {
+    let max = arr.length - 1
+    let min = 0
+    while (min < max) {
+        let mid = Math.floor((max + min) / 2)
+        if (target < arr[mid]) {
+            max = mid - 1
+        } else if (target > arr[mid]) {
+            min = mid + 1
+        } else {
+            return mid
+        }
+    }
+    return false
+}
